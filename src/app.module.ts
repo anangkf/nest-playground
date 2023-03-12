b0cb8921a3ee69856ab057528ad8ce2e6752a73e
anangkf_ <gonanggoneng@gmail.com>
2023-03-12 20:12:46 +0700

    configure mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    // configure env vars
+    ConfigModule.forRoot({
+      // enable env vars globally
+      isGlobal: true,
+    }),
+    // connect mongodb
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [],
 })
