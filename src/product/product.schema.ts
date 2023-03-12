d78b96c9499135058702c53e23bd09fe79a77f13
anangkf_ <gonanggoneng@gmail.com>
2023-03-12 23:02:46 +0700

    delete _id and __v from product response object

--- src/product/product.schema.ts
+++ src/product/product.schema.ts
@@ -5,14 +5,25 @@ export type ProductDocument = HydratedDocument<Product>;
 
 @Schema()
 export class Product {
-  @Prop()
+  @Prop({ type: String, required: true })
   name: string;
 
-  @Prop()
+  @Prop({ type: Number, required: true })
   price: number;
 
-  @Prop({ type: Date, default: new Date().toISOString() })
+  @Prop({ type: Date, default: new Date().toISOString(), required: true })
   created_at: string;
 }
 
-export const ProductSchema = SchemaFactory.createForClass(Product);
+const ProductSchema = SchemaFactory.createForClass(Product);
+
+// transform returned object
+ProductSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+export { ProductSchema };
